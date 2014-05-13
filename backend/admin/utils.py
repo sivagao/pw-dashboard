04c7d8606a152586eee299321caf10a1f6a00d66
gaohailang <gaohailang@wandoujia.com>
2014-05-14 04:18:06 +0800

    polish: update for loop into a fp style

--- backend/admin/utils.py
+++ backend/admin/utils.py
@@ -1,11 +1,8 @@
 import json
 
-json_data=open('./admin/fixture/person.json')
-person_data = json.load(json_data)
-person_dict = {}
-for i in person_data:
-    person_dict[i['id']] = i
-json_data.close()
+with open('./admin/fixture/person.json') as json_data:
+    person_data = json.load(json_data)
+    person_dict = dict(zip(map(lambda x:x["id"], person_data), person_data))
 
 def getPersonImg(id):
     return person_dict[id].get('img', 'http://img.wdjimg.com/who/avatar0.png')
